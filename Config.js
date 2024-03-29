4344721c83bdbbf160245770b2fd14c8adefd433
wepdesigner <143806324+wepdesigner@users.noreply.github.com>
2024-03-28 23:55:16 -0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['237697862259']
+global.ownername = "😎MC STEVE😎"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Cameroon"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
